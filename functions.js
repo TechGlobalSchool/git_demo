1658116cad89ca87ff9953898ba90c54f225990e
TechGlobalSchool <tgschool01@gmail.com>
2023-08-16 20:17:22 -0500

    added tests for sum functions

--- functions.js
+++ functions.js
@@ -5,3 +5,7 @@ Create a function that finds the sum of 2 numbers and returns it
 function sum(a, b){
     return a + b;
 }
+
+console.log(sum(1, 2)); // 3
+console.log(sum(1, 5)); // 6
+console.log(sum(11, -2)); // 9
